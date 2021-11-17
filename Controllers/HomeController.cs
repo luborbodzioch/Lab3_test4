bb4b2f8ef0f67e01f577153377062947fa62dfda
luborbodzioch <bodzio.lubek@gmail.com>
2021-11-17 19:44:20 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Lab3_test4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3_test4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Test2(int id, int? param2, int param3 = 30, int param4 = 40)
+        {
+            ViewData["id"] = id;
+            ViewData["p2"] = param2;
+            ViewData["p3"] = param3;
+            ViewData["p4"] = param4;
+            return View();
+        }
+
+        [Route("Home/Tescik3")]
+
+
+        public IActionResult Test3()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/FormModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+namespace Lab3_test4.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Nazwa elementu")]
+[Required]
+        public string Name { get; set; }
+        [Display(Name = "Opis elementu")]
+        public string Description { get; set; }
+        [Display(Name = "Element widoczny?")]
+        public bool IsVisible { get; set; }
+    }
+}
